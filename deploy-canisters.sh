bd20d6638bccccab66189aee152ec5feb6973bd5
Henrique Costa <hmcosta2002@gmail.com>
2024-07-10 11:18:42 +0100

    script to deploy all canisters locally

---
+++ deploy-canisters.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# REQUIREMENTS
+# - on another tab you already did "dfx start" (--clean)
+
+# HOW TO RUN:
+# Run the following command: npm run local
+
+
+dfx deploy hgb_backend
+dfx deploy sale
+dfx deploy exchange
+dfx deploy property_database
+dfx deploy HGB_token
+dfx deploy LNFT
+dfx deploy hgb_frontend
+
+echo ""
+echo "Finished Deploy Local Script"
+echo ""
