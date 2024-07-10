8921da02dc41e2a93f0eb16214bccb9924ada8b2
Nuno Costa <nuno.mmc@gmail.com>
2024-07-10 22:17:36 +0100

    WalletConnect NFID (not working)

--- src/hgb_frontend/src/App.jsx
+++ src/hgb_frontend/src/App.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './index.scss';
 
 const App = () => (
   <>
-    <div data-collapse="medium" data-animation="default" data-duration="400" data-easing="ease" data-easing2="ease"
-        role="banner" className="navigation-bar w-nav">
-      <div className="w-container navbar-w">
-        <a href="/" aria-current="page" className="brand-link w-nav-brand w--current">
-          <img src="/logo-white.svg" loading="lazy" width="360" alt="" className="image" />
-          </a>
-          <nav role="navigation" className="navigation-menu w-nav-menu"><a href="/" aria-current="page"
-                  className="navigation-link w-nav-link w--current">Sign in</a></nav>
-          <div className="hamburger-button w-nav-button">
-              <div className="w-icon-nav-menu"></div>
-          </div>
-      </div>
-    </div>
+    <Navbar/>
     <div className="hero-section centered">
       <div data-w-id="e464d218-f801-55d1-1f50-7da00b5bfb8f" className="container w-container">
         <div className="w-layout-hflex flex-block">
